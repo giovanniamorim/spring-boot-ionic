a3d78c97ee7b05c8a6aa9681013813ed5343f1b8
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-02-24 20:02:40 -0300

    Mapeamento 1 para 1 das classes Pagamento e Pedido

--- src/main/java/com/udemy/cursomc/domain/Pagamento.java
+++ src/main/java/com/udemy/cursomc/domain/Pagamento.java
@@ -3,6 +3,10 @@ package com.udemy.cursomc.domain;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 
 import com.udemy.cursomc.domain.enums.EstadoPagamento;
 
@@ -11,10 +15,15 @@ public class Pagamento implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	
 	private Integer id;
 	
 	private EstadoPagamento estado;
 	
+	@OneToOne
+	@JoinColumn(name = "pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
--- src/main/java/com/udemy/cursomc/domain/Pedido.java
+++ src/main/java/com/udemy/cursomc/domain/Pedido.java
@@ -3,10 +3,12 @@ package com.udemy.cursomc.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Pedido implements Serializable{
@@ -19,6 +21,7 @@ public class Pedido implements Serializable{
 	
 	private Date instante;
 	
+	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
 	private Pedido pedido;
