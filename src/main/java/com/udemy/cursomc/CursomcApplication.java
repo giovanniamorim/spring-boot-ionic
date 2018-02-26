35105151183302b662e6b9ae5ffb23ecd9d35546
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-02-26 17:38:47 -0300

    Implementação da busca de produtos por nome e categorias

--- src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -81,17 +81,40 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
+		Produto p3 = new Produto(null, "Notebook", 2980.00);
+		Produto p4 = new Produto(null, "Cadeira", 40.00);
+		Produto p5 = new Produto(null, "Colchão", 820.00);
+		Produto p6 = new Produto(null, "Copo", 2.00);
+		Produto p7 = new Produto(null, "Mesa de Vidor", 610.00);
+		Produto p8 = new Produto(null, "Vestido Azul", 180.00);
+		Produto p9 = new Produto(null, "Teclado", 20.00);
+		Produto p10 = new Produto(null, "Caneta", 1.10);
+		Produto p11 = new Produto(null, "Borracha", 0.50);
+		Produto p12 = new Produto(null, "Mochila", 285.00);
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat2.getProdutos().addAll(Arrays.asList(p4, p6));
+		cat3.getProdutos().addAll(Arrays.asList(p7, p8));
+		cat4.getProdutos().addAll(Arrays.asList(p9, p10));
+		cat5.getProdutos().addAll(Arrays.asList(p11));
+		cat6.getProdutos().addAll(Arrays.asList(p12, p9));
+		cat7.getProdutos().addAll(Arrays.asList(p2, p7));
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
+		p4.getCategorias().addAll(Arrays.asList(cat3));
+		p5.getCategorias().addAll(Arrays.asList(cat3));
+		p6.getCategorias().addAll(Arrays.asList(cat4));
+		p7.getCategorias().addAll(Arrays.asList(cat5));
+		p8.getCategorias().addAll(Arrays.asList(cat6));
+		p9.getCategorias().addAll(Arrays.asList(cat7));
+		p10.getCategorias().addAll(Arrays.asList(cat3));
+		p11.getCategorias().addAll(Arrays.asList(cat4));
+		p12.getCategorias().addAll(Arrays.asList(cat5));
 				
 		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10));
-		produtoRepository.save(Arrays.asList(p1, p2, p3));
+		produtoRepository.save(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
