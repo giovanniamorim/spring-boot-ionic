3e996adbfd0711afa0540540f6c81405653d7a4e
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-01 11:05:52 -0300

    Restrição de conteúdo nos pedidos

--- src/main/java/com/udemy/cursomc/resources/PedidoResource.java
+++ src/main/java/com/udemy/cursomc/resources/PedidoResource.java
@@ -5,18 +5,18 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.udemy.cursomc.domain.Categoria;
 import com.udemy.cursomc.domain.Pedido;
-import com.udemy.cursomc.domain.dto.CategoriaDTO;
 import com.udemy.cursomc.services.PedidoService;
 
 @RestController
@@ -39,5 +39,16 @@ public class PedidoResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(pedido.getId()).toUri(); 
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="12") Integer linesPerPage,
+			@RequestParam(value="direction", defaultValue="ASC") String direction,
+			@RequestParam(value="orderBy", defaultValue="nome")String orderBy){
+		Page<Pedido> list = pedidoService.findPage(page, linesPerPage, direction, orderBy);
+		return ResponseEntity.ok().body(list);
+		
 	}
 }
--- src/main/java/com/udemy/cursomc/services/PedidoService.java
+++ src/main/java/com/udemy/cursomc/services/PedidoService.java
@@ -3,8 +3,12 @@ package com.udemy.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.udemy.cursomc.domain.Cliente;
 import com.udemy.cursomc.domain.ItemPedido;
 import com.udemy.cursomc.domain.PagamentoComBoleto;
 import com.udemy.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.udemy.cursomc.repositories.ItemPedidoRepository;
 import com.udemy.cursomc.repositories.PagamentoRepository;
 import com.udemy.cursomc.repositories.PedidoRepository;
 import com.udemy.cursomc.repositories.ProdutoRepository;
+import com.udemy.cursomc.security.UserSpringSecurity;
+import com.udemy.cursomc.services.exceptions.AuthorizationException;
 import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -74,6 +80,18 @@ public class PedidoService {
 		
 		return pedido;
 	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
+		UserSpringSecurity user = UserService.authenticated();
+		if( user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
+		
+	}
+
 }
 
 
