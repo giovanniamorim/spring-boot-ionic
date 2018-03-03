e7a8be487fada9e39ff83ee2e906c2bce0eb0899
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-03 16:14:40 -0300

    Configurando template de email em html

---
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -0,0 +1,21 @@
+package com.nelioalves.cursomc.services;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido pedido);
+	
+	void sendHtmlEmail(MimeMessage msg);
+}
